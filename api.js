dabc161794ae57141765c48adb6a37bbc4d81451
KarimMenem <karimmenem@Karims-MacBook-Pro.local>
2025-06-30 15:47:04 +0300

    initializing firebase for the transition

--- api.js
+++ api.js
@@ -1,3 +1,22 @@
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: "AIzaSyC0WNvVIrXcqprzTC2WGWSn6Pi5ayXDF0U",
+  authDomain: "vanlife-3fcae.firebaseapp.com",
+  projectId: "vanlife-3fcae",
+  storageBucket: "vanlife-3fcae.firebasestorage.app",
+  messagingSenderId: "765325323966",
+  appId: "1:765325323966:web:ad87f38bf5f242abe6b8ef"
+};
+const app = initializeApp(firebaseConfig);
+
+
+
+
+
+
+
+
+
 export async function getVans(id) {
     const url = id ? `/api/vans/${id}` : "/api/vans"
     const res = await fetch(url)
